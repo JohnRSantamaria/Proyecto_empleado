18626e9a9ddf2055fa3a095324b55fb4f449de32
John Santamaria <santamariajohnd@gmail.com>
2023-09-05 09:03:18 -0500

    Configuracion inicial

--- empleado/settings/base.py
+++ empleado/settings/base.py
@@ -13,6 +13,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Local Apps
+    'applications.departamento.apps',
+    'applications.empleados.apps',
 ]
 
 MIDDLEWARE = [
